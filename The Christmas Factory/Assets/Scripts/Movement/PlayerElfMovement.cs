a040537be66a6836aa436762ada742b4ce6203bf
Gaston Caminiti <gastoncaminiti@gmail.com>
2022-05-02 11:57:01 -0300

    Player Elf Movement
    PlayerElf Prefab
    PlayerElfMovement Script
    MoveElf Method
    GetMoveValue Method

---
+++ The Christmas Factory/Assets/Scripts/Movement/PlayerElfMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerElfMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    [Range(5, 50)]
+    private float elfSpeed = 5;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveElf();
+    }
+
+    private void MoveElf()
+    {
+        float verticalMove = GetMoveValue(Input.GetAxis("Vertical"));
+        float horizontalMove = GetMoveValue(Input.GetAxis("Horizontal"));
+        transform.Translate(horizontalMove, verticalMove, 0f);
+    }
+
+    private float GetMoveValue(float axisValue)
+    {
+        return axisValue * elfSpeed * Time.deltaTime;
+    }
+}
