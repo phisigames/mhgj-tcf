0f31fe3df5e2ef66763e2c5f9e8f4ff3662cdb68
Gaston Caminiti <gastoncaminiti@gmail.com>
2022-05-02 19:20:57 -0300

    Toy Spawner
    Conveyor Prefab
    ToySpawner Script
    InstantiateToy Method

---
+++ The Christmas Factory/Assets/Scripts/Spawner/ToySpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToySpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject[] toysPrefabs;
+
+    [SerializeField]
+    [Range(0, 20)]
+    private float spawnerInitialDelay = 0f;
+
+    [SerializeField]
+    [Range(0, 20)]
+    private float spawnerRepeatRate = 0.5f;
+
+    void Start()
+    {
+        InvokeRepeating("InstantiateToy", spawnerInitialDelay, spawnerRepeatRate);
+    }
+
+    private void InstantiateToy()
+    {
+        Debug.Log("NEW TOY");
+        int enemyIndex = Random.Range(0, toysPrefabs.Length);
+        Instantiate(toysPrefabs[enemyIndex], transform);
+    }
+
+}
