42925ced0569f926b82706d17b77ad845fb9e5e3
Gaston Caminiti <gastoncaminiti@gmail.com>
2022-05-04 17:49:15 -0300

    Toy Polling Refactor
    Refactor Toy Prefab
    Refactor EndpointCollisions.cs
    Toys, Conveyors y Endpoints layers
    Delete Reposition Code in ConveyorManager.cs

--- The Christmas Factory/Assets/Scripts/Collisions/EndpointCollisions.cs
+++ The Christmas Factory/Assets/Scripts/Collisions/EndpointCollisions.cs
@@ -5,32 +5,11 @@ using UnityEngine;
 public class EndpointCollisions : MonoBehaviour
 {
 
-    [SerializeField]
-    private int endpointGap = 1;
-    public int EndpointGap { get { return endpointGap; } }
-
-    [SerializeField]
-    [Range(0.1f, 5f)]
-    private float minPosition = 1;
-
-    [SerializeField]
-    private Vector3 initialPosition;
-
-    private void Start()
-    {
-        initialPosition = transform.localPosition;
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Toys"))
         {
-            other.gameObject.GetComponent<ToyMovement>().CanMove = false;
-            other.gameObject.transform.localPosition = new Vector3(transform.localPosition.x - endpointGap/2, transform.position.y, transform.position.z);
-            if (transform.localPosition.x > minPosition)
-            {
-                transform.localPosition = new Vector3(transform.localPosition.x - endpointGap, transform.position.y, transform.position.z);
-            }
+           other.gameObject.SetActive(false);
         }
     }
 
--- The Christmas Factory/Assets/Scripts/Movements/ToyMovement.cs
+++ The Christmas Factory/Assets/Scripts/Movements/ToyMovement.cs
@@ -6,13 +6,6 @@ using UnityEngine;
 public class ToyMovement : MonoBehaviour
 {
     [SerializeField]
-    [Range(5, 50)]
-    private float toySpeed = 2;
-
-    [SerializeField] private bool canMove = true;
-    public bool CanMove { get { return canMove; } set { canMove = value; } }
-
-    [SerializeField]
     private Vector3 initialPosition;
 
     private void Awake()
@@ -20,20 +13,9 @@ public class ToyMovement : MonoBehaviour
         initialPosition = transform.position;
     }
 
-    void Update()
-    {
-        MoveToy();
-    }
-
-    private void MoveToy()
-    {
-        if (!canMove) { return; }
-        transform.Translate(Vector3.right * toySpeed * Time.deltaTime);
-    }
-
     private void OnDisable()
     {
         transform.position = initialPosition;
-        canMove = true;
+        transform.rotation = Quaternion.identity;
     }
 }
