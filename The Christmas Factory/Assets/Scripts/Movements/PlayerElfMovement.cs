aa51567ec24f51a82fd27f8b71db037d975cf260
Gaston Caminiti <gastoncaminiti@gmail.com>
2022-05-11 20:38:31 -0300

    Elf Animation: Walk and Idle
    Animation Controller: Elf
    Slide Idle Animation Clip
    Front Idle Animation Clip
    Walk Idle Animation Clip
    Coffe Idle Animation Clip
    Talk Idle Animation Clip
    Stress Idle Animation Clip

--- The Christmas Factory/Assets/Scripts/Movements/PlayerElfMovement.cs
+++ The Christmas Factory/Assets/Scripts/Movements/PlayerElfMovement.cs
@@ -7,6 +7,12 @@ public class PlayerElfMovement : MonoBehaviour
     [Range(5, 50)]
     private float elfSpeed = 5;
 
+    [SerializeField]
+    private Animator myAnimator;
+
+    [SerializeField]
+    private SpriteRenderer _renderer;
+
     void Update()
     {
         MoveElf();
@@ -16,6 +22,24 @@ public class PlayerElfMovement : MonoBehaviour
     {
         float verticalMove = GetMoveValue(Input.GetAxis("Vertical"));
         float horizontalMove = GetMoveValue(Input.GetAxis("Horizontal"));
+
+        if ((verticalMove == 0) && (horizontalMove == 0))
+        {
+            myAnimator.SetBool("isWalking", false);
+        }
+        else
+        {
+            myAnimator.SetBool("isWalking", true);
+            if (horizontalMove > 0)
+            {
+                _renderer.flipX = false;
+            }
+            else if (horizontalMove < 0)
+            {
+                _renderer.flipX = true;
+            }
+        }
+
         transform.Translate(horizontalMove, verticalMove, 0f);
     }
 
